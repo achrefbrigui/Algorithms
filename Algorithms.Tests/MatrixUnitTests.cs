a1eb03976c17aa21502f1837710b0d5d7a4b4f12
Achref <achref.brigui98@gmail.com>
2022-10-05 17:41:19 +0200

    Auto stash before rebase of "refs/heads/main"
    Matrix rotation implementation

---
+++ Algorithms.Tests/MatrixUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class MatrixUnitTests
+    {
+        [Theory]
+        [MemberData(nameof(ClockWiseData))]
+        public void ClockWiseDataUnitTest(int[,] expected, int[,] input)
+        {
+            Assert.Equal(expected, input.ClockWise());
+        }
+
+        [Theory]
+        [MemberData(nameof(AClockWiseData))]
+        public void AClockWiseDataUnitTest(int[,] expected, int[,] input)
+        {
+            Assert.Equal(expected, input.AClockWise());
+        }
+
+        public static IEnumerable<object[]> AClockWiseData =>
+           new List<object[]>
+           {
+                new object[] { new int[,] { { 3, 6, 9 }, { 2, 5, 8 }, { 1, 4, 7 } }, new int[,] {{ 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } } },
+                new object[] { new int[,] { { 3, 6 }, { 2, 5 }, { 1, 4 } }, new int[,] {{ 1, 2, 3 }, { 4, 5, 6 } } },
+           };
+
+        public static IEnumerable<object[]> ClockWiseData =>
+           new List<object[]>
+           {
+                new object[] { new int[,] { { 7, 4, 1 }, { 8, 5, 2 }, { 9, 6, 3 } }, new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } } },
+                new object[] { new int[,] {{ 4, 1 }, { 5, 2 }, { 6, 3 } }, new int[,] { { 1, 2, 3 }, { 4, 5, 6 } } },
+           };
+    }
+}
---
+++ Algorithms/MatrixExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{        
+
+    public static class MatrixExtensions
+    {    
+        //public enum ERotationType { ClockWise, AClockWise };
+
+        public static int[,] NinetyDegRotation(this int[,] input, bool isClockwise)
+        {
+            return isClockwise ? input.ClockWise() : input.AClockWise();
+        }
+
+        public static int[,] ClockWise(this int[,] input)
+        {
+            var output = new int[input.GetLength(1), input.GetLength(0)];
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    output[j, input.GetLength(0) - 1 - i] = input[i, j];
+                }
+            }
+           
+            return output;
+        }
+
+        public static int[,] AClockWise(this int[,] input)
+        {
+            var output = new int[input.GetLength(1), input.GetLength(0)];
+
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                for (int j = input.GetLength(1) - 1; j >= 0; j--)
+                {
+                    output[input.GetLength(1) - 1 - j, i] = input[i, j];
+                }
+            }
+     
+            return output;
+        }
+        
+    }
+}
