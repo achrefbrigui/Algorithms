8486891094bf8d760f400aee390d91ba207209ce
Achref <achref.brigui98@gmail.com>
2022-10-05 15:02:19 +0200

    Palyndrome implementation

---
+++ Algorithms.Tests/StringExtensionsUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class StringExtensionsUnitTests
+    {
+        [Theory]
+        [InlineData("elle", true)]
+        [InlineData("ll", true)]
+        [InlineData("a", true)]
+        [InlineData("palyndrome", false)]
+        public void IsPalyndromeUnitTest(string input, bool expected)
+        {
+            Assert.Equal(expected, input.IsPalyndrome());
+        }
+    }
+}
---
+++ Algorithms/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalyndrome(this string input)
+        {
+            if (input.Length == 1 || input.Length == 0) return true;
+            if (input.Length == 2) return input[0] == input[1];
+            if (input[0] == input[input.Length - 1]) return IsPalyndrome(input.Substring(1, input.Length - 2));
+            return false;
+        }
+    }
+}
